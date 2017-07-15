219befe6b95abb549cffb286475a635e8aecb3e7
Peter Arbanas <parbanas@hotmail.com>
2017-07-15 13:49:00 -0700

    Indeed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+  showAlert: boolean = true;
+  hideAlert(): void {
+    this.showAlert = ! this.showAlert;
+  }
 }
